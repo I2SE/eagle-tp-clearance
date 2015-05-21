a0521aa5f99bc6a08a21d2fb390f9bba4055d188
Christian Aurich (I2SE GmbH) <christian.aurich@i2se.com>
2015-05-21 10:24:02 +0200

    adding verbose prints for info and debugging

--- eagle-tp-clearance.py
+++ eagle-tp-clearance.py
@@ -3,6 +3,7 @@ import argparse
 import sys
 import math
 import itertools
+import logging as log
 
 def distance(p0, p1):
     return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
@@ -10,7 +11,24 @@ def distance(p0, p1):
 parser = argparse.ArgumentParser(description='Check eagle files for the distance between testpoints. The returned value is the distance from center to center of the testpads.')
 parser.add_argument('boardfile', action="store", type=argparse.FileType('rb'))
 parser.add_argument("-l", "--limit", help="define a value that is used as test condition for the minimum tp distance, a TP distance higher than the limit results in an error",type=float, default=0)
+parser.add_argument('--verbose', '-v', help="show more information", default=0, action='count')
 args = parser.parse_args(sys.argv[1:])
+
+
+
+if args.verbose == 0:
+  log.basicConfig(format="%(levelname)s: %(message)s", level=log.ERROR)
+elif args.verbose == 1:
+  log.basicConfig(format="%(levelname)s: %(message)s", level=log.WARNING)
+elif args.verbose == 2:
+  log.basicConfig(format="%(levelname)s: %(message)s", level=log.INFO)
+elif args.verbose >= 3:
+  log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+
+log.info("limit=" + str(args.limit))
+
+
+
 
 eagle_object = objectify.fromstring(args.boardfile.read())
 
@@ -43,10 +61,13 @@ else:
 
 if args.limit == 0:
   #no limit set, automatically succeed
+  log.debug("result code is 0")
   sys.exit(0)
 elif min_distance <= args.limit:
   #limit is not exceeded, succeed
+  log.debug("result code is 0")
   sys.exit(0)
 else:
   #limit is exceeded, fail
+  log.debug("result code is 1")
   sys.exit(1)
